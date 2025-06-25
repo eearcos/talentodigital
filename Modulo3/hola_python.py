92321b36eff563a6e154091efeb98571d2696342
eearcos <eearcosb@gmail.com>
2025-06-25 19:49:17 -0400

    Update Autofix

---
+++ Modulo3/hola_python.py
@@ -0,0 +1,17 @@
+print("¡Hola desde Python!")
+
+lenguaje = "Hola de nuevo Python"
+print(lenguaje)
+
+numero = 20
+
+print(numero)
+
+x = int(input("Introduce un número: "))
+
+par = x%2
+
+if par == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
