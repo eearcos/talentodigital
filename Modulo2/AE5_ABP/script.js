4ea7d8b3668e9fb4e8adbcfb602cfb55788c2a85
eearcos <eearcosb@gmail.com>
2025-07-23 17:34:03 -0400

    Tarea 5 individual lista

---
+++ Modulo2/AE5_ABP/script.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    const contadorSolicitudes = document.getElementById("contador-solicitudes");
+    const contadorConexiones = document.getElementById("contador-conexiones");
+
+    const botonesAceptar = document.querySelectorAll(".aceptar");
+    const botonesRechazar = document.querySelectorAll(".rechazar");
+
+    function disminuirSolicitudes() {
+
+        let actual = parseInt(contadorSolicitudes.textContent);
+        contadorSolicitudes.textContent = actual - 1;
+    }
+
+    function aumentarConexiones() {
+
+        let actual = parseInt(contadorConexiones.textContent);
+        contadorConexiones.textContent = actual + 1;
+    }
+
+    botonesAceptar.forEach(function (boton) {
+        boton.addEventListener("click", function () {
+
+            const solicitud = this.parentElement;
+
+            solicitud.remove();
+
+            disminuirSolicitudes();   
+            aumentarConexiones();     
+
+            alert("¡Solicitud aceptada!");
+
+        });
+    });
+
+    botonesRechazar.forEach(function (boton) {
+        boton.addEventListener("click", function () {
+
+            const solicitud = this.parentElement;
+
+            solicitud.remove();
+
+            disminuirSolicitudes();
+
+            console.log("Solicitud rechazada");
+        });
+    });
+
+});
