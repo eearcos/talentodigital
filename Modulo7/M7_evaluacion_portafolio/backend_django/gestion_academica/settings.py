995858d39a16584d6d0e8da5f6274f1dce26ae26
eearcos <eearcosb@gmail.com>
2025-12-17 02:24:42 -0300

    Fix: Configuración de ALLOWED_HOSTS para despliegue en Render

--- Modulo7/M7_evaluacion_portafolio/backend_django/gestion_academica/settings.py
+++ Modulo7/M7_evaluacion_portafolio/backend_django/gestion_academica/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8=85+3rgt2#q9we&^qr%e+7)!bp34dm5c_3rfvj7#at9q)8v99'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['talentodigital.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
